3a68e1c071e25aa1a9d8dba00524e37116918ad7
Roberto <robertsantos.dom@gmail.com>
2023-12-23 15:40:44 +0800

    main logic fixed, need design and buttons

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-const divBoxesSize = () => {
-    const rangeSlider = document.querySelector('#range-slider');
-    const mainContainer = document.querySelector('#main-container');
-    const boxContainer = document.querySelector('#box-container');
+const rangeSlider = document.querySelector('#range-slider');
+const boxContainer = document.querySelector('#box-container');
 
-    boxContainer.innerHTML = "";
+function createDiv(size) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
 
-    for (let i = 0; i < rangeSlider.value; i++) {
-        const box = document.createElement("div");
-        box.className = "box";
-        boxContainer.appendChild(box);
-    }
+    return div;
 }
+
+function createGrid(gridSize) {
+    const fragment = document.createDocumentFragment(); // Create a fragment to hold the elements
+
+    const cellSize = boxContainer.clientWidth / gridSize;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            fragment.appendChild(createDiv(cellSize));
+        }
+    }
+
+    boxContainer.innerHTML = ''; // Clear the container
+    boxContainer.appendChild(fragment); // Append all elements at once
+}
+
+function createGridSize() {
+    let gridSize = parseInt(rangeSlider.value);
+    if (gridSize < 2) {
+        gridSize = 2;
+    } else if (gridSize > 64) {
+        gridSize = 64;
+    }
+    createGrid(gridSize);
+}
+
+rangeSlider.addEventListener('input', createGridSize);
+createGridSize(); // Initial grid creation
