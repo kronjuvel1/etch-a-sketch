68ef8e2607bfbb9802136e4616b211da2d50d8f4
Roberto <robertsantos.dom@gmail.com>
2023-12-26 15:59:09 +0800

    v1.1 edited comments and fixed range slider

--- script.js
+++ script.js
@@ -6,20 +6,23 @@ const eraseMode = document.querySelector('#erase-mode');
 const clearBtn = document.querySelector('#clear');
 const colorPicker = document.querySelector('#color-picker');
 
-const createDiv = size => {
+// Create a div element with the given size in pixels and return it
+const createDiv = (size) => {
     const div = document.createElement('div');
     div.classList.add('box');
     div.style.width = `${size}px`;
     div.style.height = `${size}px`;
 
-    div.addEventListener('dragstart', function(event) {
+    // Prevent dragging of divs
+    div.addEventListener('dragstart', function (event) {
         event.preventDefault();
     });
 
     return div;
 };
 
-const createGrid = gridSize => {
+// Create a grid of divs with the given size in pixels
+const createGrid = (gridSize) => {
     const fragment = document.createDocumentFragment();
     const cellSize = boxContainer.clientWidth / gridSize;
 
@@ -33,29 +36,48 @@ const createGrid = gridSize => {
     boxContainer.appendChild(fragment);
 };
 
+// Create a grid with the size of the range slider
 const createGridSize = () => {
     let gridSize = parseInt(rangeSlider.value);
     gridSize = Math.min(Math.max(gridSize, 2), 64);
     createGrid(gridSize);
 };
 
+// Apply a given callback to each box based on mouse events
+const applyMouseEvent = (callback) => {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener('click', () => {
+            callback(box);
+        });
+        box.addEventListener('mousedown', () => {
+            callback(box);
+        });
+        box.addEventListener('mouseover', (event) => {
+            if (event.buttons === 1) {
+                callback(box);
+            }
+        });
+    });
+};
+
 const changeColorMode = () => {
     const selectedColor = colorPicker.value;
-    const paintBox = box => {
+    const paintBox = (box) => {
         box.style.backgroundColor = selectedColor;
     };
     applyMouseEvent(paintBox);
 };
 
 const changeRainbowMode = () => {
-    const paintBox = box => {
+    const paintBox = (box) => {
         box.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
     };
     applyMouseEvent(paintBox);
 };
 
 const changeEraseMode = () => {
-    const paintBox = box => {
+    const paintBox = (box) => {
         box.style.backgroundColor = 'white';
     };
     applyMouseEvent(paintBox);
@@ -63,39 +85,22 @@ const changeEraseMode = () => {
 
 const clearGrid = () => {
     const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {
+    boxes.forEach((box) => {
         box.style.backgroundColor = 'white';
     });
 };
 
 const randomGrid = () => {
-    const paintBox = box => {
+    const paintBox = (box) => {
         box.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
     };
     applyMouseEvent(paintBox);
 };
 
-const applyMouseEvent = callback => {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {
-        box.addEventListener('click', () => {
-            callback(box);
-        });
-        box.addEventListener('mousedown', () => {
-            callback(box);
-        });
-        box.addEventListener('mouseover', event => {
-            if (event.buttons === 1) {
-                callback(box);
-            }
-        });
-
-    });
-};
-
-document.querySelectorAll('.btn').forEach(btn => {
-    btn.addEventListener('click', function() {
-        document.querySelectorAll('.btn').forEach(otherBtn => {
+// makes sure only one button is active at a time
+document.querySelectorAll('.btn').forEach((btn) => {
+    btn.addEventListener('click', function () {
+        document.querySelectorAll('.btn').forEach((otherBtn) => {
             if (otherBtn !== this && otherBtn.id !== 'clear') {
                 otherBtn.classList.remove('active');
             }
